c3bd19d192352e921b930f5701dffccbc9519813
Wallace Saturnino <wallace@MacBook-Pro-de-Wallace.local>
2019-08-20 16:16:05 -0300

    Finalizando o aplicativo. Listagem das organizações do usuário

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {View, FlatList, ActivityIndicator} from 'react-native';
 import PropTypes from 'prop-types';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import api from '~/services/api';
 import Header from '~/components/Header';
+import styles from './styles';
 
-const Organizations = () => (
-  <View>
-    <Header title="Organizações" />
-  </View>
-);
+import OrganizationItem from './OrganizationItem';
 
 const TabIcon = ({tintColor}) => (
   <Icon name="building" size={20} color={tintColor} />
@@ -19,8 +18,58 @@ TabIcon.propTypes = {
   tintColor: PropTypes.string.isRequired,
 };
 
-Organizations.navigationOptions = {
-  tabBarIcon: TabIcon,
-};
+export default class Organizations extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
 
-export default Organizations;
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
+  };
+
+  async componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    this.setState({refreshing: true});
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const {data} = await api.get(`/users/${username}/orgs`);
+
+    this.setState({data, loading: false, refreshing: false});
+  };
+
+  renderListItem = ({item}) => <OrganizationItem organization={item} />;
+
+  renderList = () => {
+    const {data, refreshing} = this.state;
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        onRefresh={this.loadOrganizations}
+        refreshing={refreshing}
+        numColumns={2}
+        columnWrapperStyle={styles.columnWrapper}
+      />
+    );
+  };
+
+  render() {
+    const {loading} = this.state;
+    return (
+      <View style={styles.container}>
+        <Header title="Organizações" />
+        {loading ? (
+          <ActivityIndicator style={styles.loading} />
+        ) : (
+          this.renderList()
+        )}
+      </View>
+    );
+  }
+}
