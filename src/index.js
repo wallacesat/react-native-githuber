a00ccc90cbd308389441bc44afe7b53a6c95ff2b
Wallace Saturnino <wallace@MacBook-Pro-de-Wallace.local>
2019-08-20 10:57:30 -0300

    Defininfo rota inicial; Reconhecendo usuário no AsyncStorage

--- src/index.js
+++ src/index.js
@@ -1,43 +1,28 @@
-import React from 'react';
-import {StyleSheet, ScrollView, View, Text} from 'react-native';
+import React, {Component} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import '~/config/ReactotronConfig';
 
-import Routes from './routes';
+import createNavigator from './routes';
 
-const App = () => (
-  // <View style={styles.container}>
-  //   <ScrollView>
-  //     <View style={styles.card}>
-  //       <Text style={styles.cardTitle}>Hellooo Guys!</Text>
-  //     </View>
-  //   </ScrollView>
-  // </View>
-  <Routes />
-);
+export default class App extends Component {
+  state = {
+    userChecked: false,
+    userLogged: false,
+  };
 
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    backgroundColor: '#a6dd',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  card: {
-    display: 'flex',
-    minWidth: '85%',
-    height: 80,
-    backgroundColor: '#fff',
-    borderRadius: 6,
-    marginTop: 80,
-    padding: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  cardTitle: {
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-});
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
 
-export default App;
+    this.setState({userChecked: true, userLogged: !!username});
+  }
+
+  render() {
+    const {userChecked, userLogged} = this.state;
+
+    if (!userChecked) return null;
+
+    const Routes = createNavigator(userLogged);
+    return <Routes />;
+  }
+}
